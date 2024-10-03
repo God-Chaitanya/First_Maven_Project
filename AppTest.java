1b4894b03735a7582f8754d7263ecc45cd98f75f
God-Chaitanya <142100932+God-Chaitanya@users.noreply.github.com>
2024-10-03 21:45:16 +0530

    Delete AppTest.java

--- AppTest.java
+++
@@ -1,8 +0,0 @@
-package net.javaguides.simpleproject;
-import org.junit.Assert;
-import org.junit.Test;
-public class AppTest {
-	public void test(){
-		Assert.assertEquals("Hello Maven", new String("Hello Maven"));
-	}
-}
